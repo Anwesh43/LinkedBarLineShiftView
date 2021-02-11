ae25214d0b0533d077ea7a1edc9fe2e00eb6bacd
Anwesh43 <anweshthecool0@gmail.com>
2021-02-11 13:25:11 +0530

    adding Canvas extension methods

--- barshiftlineview/src/main/java/com/example/barshiftlineview/BarShiftLineView.kt
+++ barshiftlineview/src/main/java/com/example/barshiftlineview/BarShiftLineView.kt
@@ -29,3 +29,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBarShiftLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val barH : Float = Math.min(w, h) / barHFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        drawLine(w * 0.5f * sf3, 0f, w * 0.5f * sf3, h * sf1 * 0.5f, paint)
+        drawRect(RectF(0f, (h - barH), w * 0.5f * (sf2 - sf3), barH), paint)
+        drawRect(RectF(0f, 0f, w * 0.5f * sf3, h / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarShiftLine(scale, w, h, paint)
+}
