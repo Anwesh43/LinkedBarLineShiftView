890145c90ed00620a95161346f37cb79ca95bee4
Anwesh43 <anweshthecool0@gmail.com>
2021-02-11 13:29:19 +0530

    adding BarShiftLineView class

--- barshiftlineview/src/main/java/com/example/barshiftlineview/BarShiftLineView.kt
+++ barshiftlineview/src/main/java/com/example/barshiftlineview/BarShiftLineView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawBSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarShiftLine(scale, w, h, paint)
 }
+
+class BarShiftLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
