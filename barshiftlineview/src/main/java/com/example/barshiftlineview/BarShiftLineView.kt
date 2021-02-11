9309a7a7490f69776ea2799b50a49bc8858e9919
Anwesh43 <anweshthecool0@gmail.com>
2021-02-11 13:14:01 +0530

    adding constants and libraroes

--- barshiftlineview/src/main/java/com/example/barshiftlineview/BarShiftLineView.kt
+++ barshiftlineview/src/main/java/com/example/barshiftlineview/BarShiftLineView.kt
@@ -1,2 +1,26 @@
 package com.example.barshiftlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#673AB7",
+    "#009688"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val strokeFactor : Float = 90f
+val barHFactor : Float = 11.2f
